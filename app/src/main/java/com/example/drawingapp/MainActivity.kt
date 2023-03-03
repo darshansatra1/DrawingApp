90735e3f72be4048e51996bba0b18d5201568d0a
Darshan Satra <darshansatra1@gmail.com>
2023-03-03 18:06:49 -0500

    add a frame layout

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,16 +4,33 @@ package com.example.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView:DrawingView? = null
+    private var imageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        imageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        imageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+        )
+
+
+
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
@@ -43,6 +60,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         brushDialog.show()
+
+    }
+
+    fun paintClicked(view:View){
+        if(view!=imageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            )
+            imageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+            )
+            imageButtonCurrentPaint = view
+        }
 
     }
 }
